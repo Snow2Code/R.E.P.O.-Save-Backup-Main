99df5032f36ab1928c8778e851ac88b674f83b32
Snow2Code <snow2code@protonmail.com>
2025-05-28 23:28:07 +0100

    Update AssemblyInfo.cs

--- Program/Properties/AssemblyInfo.cs
+++ Program/Properties/AssemblyInfo.cs
@@ -21,8 +21,8 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("35b5b6ca-ef49-4cc3-8435-9b21c4ca23b9")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 // Version information for an assembly consists of the following four values:
 //
